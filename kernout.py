80fa3294611c73f48fb81842be9dd326da478ccb
Joshua Johns <joshcjohns@gmail.com>
2015-07-22 20:42:26 -0400

    Added argument parser and args.

--- kernout.py
+++ kernout.py
@@ -4,10 +4,25 @@ import sys
 import os
 import time
 import random
+import argparse
 from pygments.lexers.c_cpp import CLexer
 from pygments.formatters import TerminalFormatter
 from pygments import highlight
 
+parser = argparse.ArgumentParser(description="Print kernel sources to standard\
+											  output.")
+group = parser.add_mutually_exclusive_group()
+parser.add_argument("-c", "--colorless", action="store_true", help="Disable\
+                    syntax highlighting")
+parser.add_argument("-m", "--multiple", type=int, help="Specify how many times\
+                    the source file is printed. (Default is 100)")
+parser.add_argument("-d", "--delay", type=float, help="Specify a consistent\
+                    delay in seconds between each line printed.")
+group.add_argument("-t", "--throwback", action="store_true", help="Print\
+                   random source file from kernel 0.01 instead of 1.0")
+group.add_argument("-f", "--file", type=file, help="Print a C source file of\
+                   your choice rather than a kernel file.")
+parser.parse_args()
 
 def random_file():
     """Return a random source file from the specified directory."""
